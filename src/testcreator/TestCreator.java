dbb842d899fd4566f9225690fae1e83c5f8b7a2a
Phùng Đức Nhật <ducnhatphung@gmail.com>
2016-10-09 23:40:20 +0700

    add app component

--- src/testcreator/TestCreator.java
+++ src/testcreator/TestCreator.java
@@ -19,7 +19,7 @@ public class TestCreator extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("AppComponent.fxml"));
         
         Scene scene = new Scene(root);
         
