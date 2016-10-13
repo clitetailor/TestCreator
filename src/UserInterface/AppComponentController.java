7a51a8f337e6c83db1d6119e84d05136fc3646e5
Phùng Đức Nhật <ducnhatphung@gmail.com>
2016-10-13 18:28:54 +0700

    hot fix

---
+++ src/UserInterface/AppComponentController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UserInterface;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author ducnh
+ */
+public class AppComponentController implements Initializable
+{
+    @FXML
+    public Button button;
+    
+    @FXML
+    public void onButtonClick()
+    {
+	
+    }
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+	// TODO
+    }    
+    
+}
